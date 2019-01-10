fdc51bb330eee77a30e8f2c99517aa1855d4b723
tyj <420785022@qq.com>
2019-01-10 22:33:50 +0800

    添加durid数据源监控页面

--- src/main/java/com/im/properties/DataSourceConfigFirst.java
+++ src/main/java/com/im/properties/DataSourceConfigFirst.java
@@ -1,13 +1,12 @@
 package com.im.properties;
 
 
-
+import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -16,6 +15,8 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import tk.mybatis.spring.annotation.MapperScan;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * Created by vostor on 2018/12/29.
@@ -23,21 +24,42 @@ import javax.sql.DataSource;
 @Configuration
 @MapperScan(basePackages = "com.im.mapper.first", sqlSessionTemplateRef = "firstSqlSessionTemplate")
 public class DataSourceConfigFirst {
-    @Bean(name = "firstDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource.first")
     @Primary
-    public DataSource setDataSource() {
-        return DataSourceBuilder.create().build();
+    @Bean(name = "firstDataSource", destroyMethod = "close", initMethod = "init")
+    @ConfigurationProperties(prefix = "spring.datasource.first")
+    public DruidDataSource setDataSource() throws SQLException {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setUsername("root");
+        druidDataSource.setPassword("123456");
+        druidDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        druidDataSource.setUrl("jdbc:mysql://localhost:3306/power_manager?useSSL=false");
+        druidDataSource.setInitialSize(2);
+        druidDataSource.setMaxActive(20);
+        druidDataSource.setMaxWait(30000);
+        druidDataSource.setTimeBetweenEvictionRunsMillis(30000);
+        druidDataSource.setMinEvictableIdleTimeMillis(30000);
+        druidDataSource.setValidationQuery("select 1");
+        druidDataSource.setTestWhileIdle(true);
+        druidDataSource.setTestOnBorrow(false);
+        druidDataSource.setTestOnReturn(false);
+        druidDataSource.setPoolPreparedStatements(false);
+        Properties properties = new Properties();
+        properties.put("druid.stat.slowSqlMillis", "10000");
+        properties.put("druid.stat.logSlowSql", "true");
+        properties.put("druid.stat.mergeSql", "true");
+        druidDataSource.setConnectProperties(properties);
+        druidDataSource.setFilters("stat,wall,log4j");
+        return druidDataSource;
     }
 
-    @Bean(name = "firstTransactionManager")
     @Primary
-    public DataSourceTransactionManager setTransactionManager(@Qualifier("firstDataSource") DataSource dataSource) {
+    @Bean(name = "firstTransactionManager")
+    public DataSourceTransactionManager setTransactionManager(@Qualifier("firstDataSource") DruidDataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
 
-    @Bean(name = "firstSqlSessionFactory")
     @Primary
+    @Bean(name = "firstSqlSessionFactory")
     public SqlSessionFactory setSqlSessionFactory(@Qualifier("firstDataSource") DataSource dataSource) throws Exception {
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
         bean.setDataSource(dataSource);
@@ -45,8 +67,8 @@ public class DataSourceConfigFirst {
         return bean.getObject();
     }
 
-    @Bean(name = "firstSqlSessionTemplate")
     @Primary
+    @Bean(name = "firstSqlSessionTemplate")
     public SqlSessionTemplate setSqlSessionTemplate(@Qualifier("firstSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
         return new SqlSessionTemplate(sqlSessionFactory);
     }
--- src/main/java/com/im/properties/DataSourceConfigSecond.java
+++ src/main/java/com/im/properties/DataSourceConfigSecond.java
@@ -1,11 +1,11 @@
 package com.im.properties;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -13,6 +13,8 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import tk.mybatis.spring.annotation.MapperScan;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * Created by vostor on 2018/12/29.
@@ -20,10 +22,32 @@ import javax.sql.DataSource;
 @Configuration
 @MapperScan(basePackages = "com.im.mapper.second", sqlSessionTemplateRef = "secondSqlSessionTemplate")
 public class DataSourceConfigSecond {
-    @Bean(name = "secondDataSource")
+    @Bean(name = "secondDataSource", destroyMethod = "close", initMethod = "init")
     @ConfigurationProperties(prefix = "spring.datasource.second")
-    public DataSource setDataSource() {
-        return DataSourceBuilder.create().build();
+    public DruidDataSource setDataSource() throws SQLException {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setUsername("root");
+        druidDataSource.setPassword("123456");
+        druidDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        druidDataSource.setUrl("jdbc:mysql://localhost:3306/power_manager_user?useSSL=false");
+        druidDataSource.setInitialSize(1);
+        druidDataSource.setMaxActive(20);
+        druidDataSource.setMaxWait(30000);
+        druidDataSource.setTimeBetweenEvictionRunsMillis(30000);
+        druidDataSource.setMinEvictableIdleTimeMillis(30000);
+        druidDataSource.setValidationQuery("select 1");
+        druidDataSource.setTestWhileIdle(true);
+        druidDataSource.setTestOnBorrow(false);
+        druidDataSource.setTestOnReturn(false);
+        druidDataSource.setPoolPreparedStatements(false);
+        Properties properties = new Properties();
+        properties.put("druid.stat.slowSqlMillis", "10000");
+        properties.put("druid.stat.logSlowSql", "true");
+        properties.put("druid.stat.mergeSql", "true");
+        properties.put("druid.stat.aop-patterns", "com.im.mapper.*");
+        druidDataSource.setConnectProperties(properties);
+        druidDataSource.setFilters("stat,wall,log4j");
+        return druidDataSource;
     }
 
     @Bean(name = "secondTransactionManager")
