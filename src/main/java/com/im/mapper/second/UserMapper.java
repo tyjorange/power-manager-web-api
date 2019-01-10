13b9b7a27dd3cdb92069d425cf26962011a1c3df
TYJ <420785022@qq.com>
2019-01-10 15:32:16 +0800

    删除无用mapper

--- src/main/java/com/im/mapper/second/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.im.mapper.second;
-
-import com.im.pojo.second.User;
-import com.im.utils.MyMapper;
-
-public interface UserMapper extends MyMapper<User> {
-}
--- src/main/java/com/im/mapper/second/UserRoleMapper.java
+++
@@ -1,7 +0,0 @@
-package com.im.mapper.second;
-
-import com.im.pojo.second.UserRole;
-import com.im.utils.MyMapper;
-
-public interface UserRoleMapper extends MyMapper<UserRole> {
-}
