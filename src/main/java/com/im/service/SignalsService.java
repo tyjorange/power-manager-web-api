ffb96ba3f62332504390192c8a821bee9c7cf4d6
TYJ <420785022@qq.com>
2019-01-17 16:12:56 +0800

    数据库 分区，分页查询

--- src/main/java/com/im/service/SignalsService.java
+++ src/main/java/com/im/service/SignalsService.java
@@ -24,11 +24,6 @@ import java.util.List;
 public class SignalsService {
     @Autowired
     private SignalsMapper signalsMapper;
-    //    @Autowired
-//    private SwitchMapper switchMapper;
-//    @Autowired
-//    private CollectorMapper collectorMapper;
-//    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
     private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
 
     /**
@@ -44,8 +39,7 @@ public class SignalsService {
     public PageInfo<HisData> findBySignalsTypesAndSwitchs(List<String> switchIds, List<String> signalsType, String startTime, String endTime, Integer page, Integer limit) {
         LocalDateTime t1 = getDateTimeOfTimestamp(Long.valueOf(startTime));
         LocalDateTime t2 = getDateTimeOfTimestamp(Long.valueOf(endTime));
-//        LocalDate parse1 = LocalDate.parse(format1, dateTimeFormatter);
-//        LocalDate parse2 = LocalDate.parse(format2, dateTimeFormatter);
+
         List<String> pp = new ArrayList<>();
         LocalDateTime temp = t1.plusDays(0);//copy
         while (temp.isBefore(t2)) {
@@ -54,27 +48,6 @@ public class SignalsService {
         }
         pp.add("p" + t2.getDayOfYear());
         System.out.println(pp);
-//        int dayOfYear1 = t1.getDayOfYear();
-//        int dayOfYear2 = t2.getDayOfYear();
-//        System.out.println(dayOfYear1 % 366);
-//        System.out.println(dayOfYear2 % 366);
-
-        /*LocalDate date = LocalDate.parse(time, DATEFORMAT);
-        // Signal记录按DAYOFYEAR(time)%366进行分区。
-        int partitonId = date.getDayOfYear() % 366; // 这天的记录在哪个分区。
-        String sql = "select * from signals PARTITION (p" + partitonId
-                + ") where switchID = '" + switchID + "' and signalsTypeID = '"
-                + signalTypeID + "' and YEAR(time) = " + date.getYear()
-                + " order by time asc";*/
-
-        /*switchIds.forEach(sw -> signalsMapper.getSignalsByTimeAndId(sw, format1, format2).forEach(hisData -> {
-                    Switch aSwitch = switchMapper.selectByPrimaryKey(sw);
-                    hisData.setSwitchName(aSwitch.getName());
-                    hisData.setCollectorName(collectorMapper.selectByPrimaryKey(aSwitch.getCollectorid()).getName());
-                    hisData.setDataTime(hisData.getTime().getTime());
-                    res.add(hisData);
-                })
-        )*/
 
         String format1 = dateTimeFormatter.format(t1);
         String format2 = dateTimeFormatter.format(t2);
